ea536b91e7ba0b1ad4229a4add365ddf66bb693c
dharsha27 <dharshavardhan27@gmail.com>
2026-08-04 23:48:05 +0530

    oil and karam added

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,10 @@
 
 
 output "vpc_id" {
-    value =aws_vpc.main.id
-
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
+
 
 output "public_subnet_ids"{
     value = aws_subnet.public[*].id
